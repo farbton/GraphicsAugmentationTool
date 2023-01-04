b19d3c6d1fbb59e3dc9386b20e7c63bbfd56ee0c
Kirko Große <farbton@gmx.net>
2023-01-04 14:01:46 +0100

    brightness überarbeitet
    manipulation von einem bzw. allen Bildern geht jetzt, sowie das abspeichern von einem bzw. allen Bildern

--- mainwindow.py
+++ mainwindow.py
@@ -5,52 +5,74 @@ Created on Sat Nov 26 20:23:50 2022
 @author: Kirko
 """
 from PyQt5 import QtCore, uic
-from PyQt5.QtWidgets import QMainWindow, QGraphicsScene, QFileDialog, QGraphicsPixmapItem, QGridLayout, QLabel, QGraphicsItem
-from PyQt5.QtGui import QPixmap, QPen
+from PyQt5.QtWidgets import QMainWindow, QGraphicsScene, QFileDialog, QGraphicsPixmapItem, QGridLayout, QLabel, QGraphicsItem, QGraphicsSimpleTextItem
+from PyQt5.QtGui import QPixmap, QPen, QFont
 from PIL import Image, ImageEnhance, ImageQt
 import reader, writer
 
-destination_folder_path = "destination/"
+
 class Window(QMainWindow):
     
     def __init__(self, app):       
         super(Window, self).__init__()       
         uic.loadUi("AugmentationTool_GUI.ui", self)
+        self.destination_folder_path = "destination/"
         self.setWindowTitle("Augmentation Tool for Images")
         self.screen = app.primaryScreen()
-        self.width = self.screen.size().width()/2
+        self.width  = self.screen.size().width()/2
         self.height = self.screen.size().height()/2
-        self.left = self.screen.size().width()/2 - self.width/2
-        self.top  = self.screen.size().height()/2 - self.height/2
+        self.left   = self.screen.size().width()/2 - self.width/2
+        self.top    = self.screen.size().height()/2 - self.height/2
         self.setGeometry(self.left, self.top, self.width, self.height)
-        print(self.size())
         self.lw_sourcefolder.resize(50,50)
-        print(self.lw_sourcefolder.size())
         self.reader = reader.Reader()
-        self.writer = writer.Writer(self, destination_folder_path)
         self.scene = QGraphicsScene(self)
+        self.info_text  = True
+        self.info2_text = False
         self.pil_imagelist = []
         self.txt_list = []
         self.pil_imagelist_bright = []
-        self.steps = 10
+        self.mode = None
         # self.setMouseTracking(True)
         # self.lw_sourcefolder.setMouseTracking(True)
         # self.grid_layout = QGridLayout()
         
+    def print_info_text_in_gv(self):
+        self.scene.addText("Drücken Sie den Button <Sourcefolder> um das Quellverzeichnis auszuwählen",
+                           font=QFont("Times", 14))
+        self.gv_preview.setScene(self.scene)
+        
+    def print_info2_text_in_gv(self):
+        self.scene.clear()
+        self.scene.addText("Wählen Sie jetzt ein Button für die Bildmanipulation aus", 
+                           font=QFont("Times", 14))
+        self.gv_preview.setScene(self.scene)
         
     def start(self):
         self.pb_sourcefolder.clicked.connect(self.open_sourcefolder)
-        self.pb_brightness.clicked.connect(self.preview_brighness)
+        self.pb_brightness.clicked.connect(self.preview_brightness_oneImage)
+        self.pb_convert_and_save_one_image.clicked.connect(self.save_one_file)
         self.pb_convert_and_save_all_images.clicked.connect(self.save_all_files)
         self.lw_sourcefolder.itemClicked.connect(self.preview_item)
+        self.pb_destinationfolder.clicked.connect(self.open_destinationfolder)
+
         
     def open_sourcefolder(self):
+        self.info_text  = False
+        self.info2_text = True
         self.source_path = QFileDialog.getExistingDirectory()
         if self.source_path:
+            self.writer = writer.Writer(self, self.source_path, self.destination_folder_path)
             self.list_filenames = self.reader.read_sourcefolder(self.source_path)
-            self.write_filenames_in_listwidget()
+            self.show_filenames_in_listwidget_sourcefolder()
+        self.print_info2_text_in_gv()
+        
+    def open_destinationfolder(self):
+        self.destination_folder_path = QFileDialog.getExistingDirectory()
+        if self.destination_folder_path:
+            print(self.destination_folder_path)
                
-    def write_filenames_in_listwidget(self):
+    def show_filenames_in_listwidget_sourcefolder(self):
         self.lw_sourcefolder.clear()
         for name in self.list_filenames:
             if name.endswith(('.jpg', '.png')):
@@ -58,9 +80,13 @@ class Window(QMainWindow):
             if name.endswith('.txt'):
                 self.txt_list.append(name)
         self.lw_sourcefolder.setCurrentRow(0)
+        self.determine_finished_images()
         
-    def preview_brighness(self):
+    def preview_brightness_oneImage(self):
+        self.mode = "brightness"
+        self.info2_text = False
         self.pil_imagelist_bright = []
+        self.steps = int(self.le_steps.text())
         factor = round((2 / self.steps), 1)
         value  = factor
         current_index = self.lw_sourcefolder.currentRow()
@@ -69,19 +95,40 @@ class Window(QMainWindow):
         # self.lb_console.setText(str(current_index))
         item_name = self.lw_sourcefolder.item(current_index)
         item_path = self.source_path + "/" + item_name.text()
+        # print(item_path)
         pil_image = Image.open(item_path)
         for j in range(self.steps):
-            img_bright = self.change_brighness(pil_image, value)
-            self.pil_imagelist_bright.append([img_bright, value])
-            value += round(factor,1)
-            print(value)
+            img_bright = self.change_brightness_oneImage(pil_image, value)
+            self.pil_imagelist_bright.append([img_bright,
+                                              item_name.text(),
+                                              value])
+            value = round(value + factor, 1)
+            # print(value)
             
         # pixmap_item = QGraphicsPixmapItem(img_pixmap)
         # self.gv_pil_imagelist.append(pil_image)
         # current_index += 1
         self.show_pil_imagelist_bright_in_gv()
 
-    def change_brighness(self, pil_image, factor):
+    def change_brightness_allImages(self):
+        self.pil_imagelist_bright_allImages = []
+        for index in range(len(self.lw_sourcefolder)):
+            self.steps = int(self.le_steps.text())
+            factor = round((2 / self.steps), 1)
+            value  = factor
+            print(factor)
+            item_name = self.lw_sourcefolder.item(index)
+            item_path = self.source_path + "/" + item_name.text()
+            pil_image = Image.open(item_path)
+
+            for j in range(self.steps):
+                img_bright = self.change_brightness_oneImage(pil_image, value)
+                self.pil_imagelist_bright_allImages.append([img_bright,
+                                                            item_name.text(),
+                                                            value])
+                value = round(value + factor, 1)
+
+    def change_brightness_oneImage(self, pil_image, factor):
         img_ieb    = ImageEnhance.Brightness(pil_image)
         img_bright = img_ieb.enhance(factor)
         return img_bright
@@ -91,13 +138,13 @@ class Window(QMainWindow):
 
     def show_pil_imagelist_bright_in_gv(self):
         self.scene.clear()
-        print("len(pil_imagelist_bright): " , len(self.pil_imagelist_bright))
-        print("len(txt_list): " , len(self.txt_list))
+        # print("len(pil_imagelist_bright): " , len(self.pil_imagelist_bright))
+        # print("len(txt_list): " , len(self.txt_list))
         gv_width, gv_height = self.get_gv_height_and_width()
         
         row = 0
         col = 0
-        for i, (image, value) in enumerate(self.pil_imagelist_bright):
+        for i, (image, name, value) in enumerate(self.pil_imagelist_bright):
             if i % 5 == 0 :
                 row += 1
                 col  = 0
@@ -116,8 +163,18 @@ class Window(QMainWindow):
             self.gv_preview.setScene(self.scene)
             col += 1
 
-    def save_all_files(self):
+    def save_one_file(self):
         self.writer.write_files_to_disk(self.pil_imagelist_bright, self.txt_list)
+        
+    def save_all_files(self):
+        self.change_brightness_allImages()
+        self.writer.write_files_to_disk(self.pil_imagelist_bright_allImages,
+                                        self.txt_list)
+    
+    def determine_finished_images(self):    
+        count_listwidget = len(self.lw_sourcefolder)
+        summe = count_listwidget * int(self.le_steps.text())
+        self.lb_sum_of_images.setText(str(summe))
 
     def mousePressEvent(self, event):
         pos = event.localPos()
@@ -126,8 +183,15 @@ class Window(QMainWindow):
     def resizeEvent(self, event):
         self.show_pil_imagelist_bright_in_gv()
         
+        if self.info_text == True:
+            self.print_info_text_in_gv()
+            
+        if self.info2_text == True:
+            self.print_info2_text_in_gv()
+        
     def preview_item(self):
-        self.preview_brighness()
+        if self.mode == "brightness":
+            self.preview_brightness_oneImage()
 
 
 
--- writer.py
+++ writer.py
@@ -7,20 +7,25 @@ Created on Fri Dec  9 13:15:35 2022
 import os
 
 class Writer():
-    def __init__(self, mainwindow, destination_folder_path):
+    def __init__(self, mainwindow, source_folder_path, destination_folder_path):
         self.mainwindow = mainwindow
+        self.source_folder_path = source_folder_path
         self.destination_folder_path = destination_folder_path
+        
     
-    def write_files_to_disk(self, imagelist, txtlist):
-        file_name = "test"
-        file_ext  = ".jpg"
-        for (image, value) in imagelist:
-            new_file_name = file_name + "_" + str(value) + file_ext
+    def write_files_to_disk(self, pil_imagelist_bright, txtlist):
+        print(pil_imagelist_bright)
+        print(txtlist)
+        # file_ext  = ".jpg"
+        for (image, fullname, value) in pil_imagelist_bright:
+            head, tail = os.path.splitext(fullname)
+            new_file_name = head + "_" + str(value) + tail
             new_file_path = self.destination_folder_path + "\\" + new_file_name
             image.save(new_file_path)
-            # with open("")  
+            self.write_txtfile_to_disk(fullname, value)
+            # with open(name)  
     
-    def write_txtfile_to_disk(self, txtlist):
+    def write_txtfile_to_disk(self, fullname, value):
         if not os.path.exists(self.destination_folder_path):
             os.makedirs(self.destination_folder_path)
             self.mainwindow.lb_console.setText("destination folder created")
@@ -28,4 +33,10 @@ class Writer():
         if not os.access(self.destination_folder_path, os.W_OK):
             print("%s folder is not writeable.")
 
-        
+        head, tail = os.path.splitext(fullname)
+        new_filename = head + "_" + str(value) + ".txt"
+        new_file_path = self.destination_folder_path + "\\" + new_filename
+        with open(self.source_folder_path + "/"+  head + ".txt", "r") as txtfile:
+            print(txtfile)
+            with open(new_file_path, "w") as new_txtfile:
+                new_txtfile.writelines(str(line) for line in txtfile)
